e969e6c2aee64fd1e665a476557cf03633d5c935
Kayyo <Sullyman08@gmail.com>
2026-03-13 14:09:55 -0400

    Wahid

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -1,5 +1,19 @@
+int len_of(char *s) {
+    int i;
+    for (i = 0; s && *s != 0; ++i) {
+        ++s;
+    }
+
+    return i;
+}
+
 void entry(void) {
     char *video_memory = (char*)0xb8000;
 
-    *video_memory = '@';
+    char *out = "This OS says that wahid a bitch";
+    int len = len_of(out);
+    for (int i = 0; i < len; ++i) {
+        video_memory[i * 2] = out[i];
+        video_memory[i * 2 + 1] = 0x0F;
+    }
 }
